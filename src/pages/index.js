8a7082f54d3f5f8116e1977d5a23b8e991667115
Jonathon Flores <jonathon@Jonathons-MacBook-Pro.local>
2018-11-19 08:07:52 -0600

    fixed cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,77 +9,61 @@ const IndexPage = props => {
   const postList = props.data.allMarkdownRemark
   return (
     <Layout>
-      <head>
-        {' '}
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />{' '}
-      </head>
-      <body>
-        <Header
-          title="The Benjamin Project"
-          subTitle="Biblical Manhood Class and
+      <Header
+        title="The Benjamin Project"
+        subTitle="Biblical Manhood Class and
   Fellowship"
-        />
-        <div className="width">
-          <div className="main-content">
-            <div className="text">
-              <h3>Header Title</h3>
-              <p>
-                {' '}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
-                facilis quo. Ducimus repellendus distinctio quia! Recusandae
-                suscipit, repellendus eum ipsum neque sequi dolorum f ugiat,
-                explicabo assumenda vero accusantium consequatur animi? Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Autem,
-                facilis quo. Ducimus repellendus distinctio quia! Recusandae
-                suscipit, repellendus eum ipsum neque sequi dolorum f ugiat,
-                explicabo assumenda vero accusantium consequatur animi?
-              </p>
-            </div>
+      />
+      <div className="width">
+        <div className="main-content">
+          <div className="text">
+            <h3>Header Title</h3>
+            <p>
+              {' '}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+              facilis quo. Ducimus repellendus distinctio quia! Recusandae
+              suscipit, repellendus eum ipsum neque sequi dolorum f ugiat,
+              explicabo assumenda vero accusantium consequatur animi? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Autem, facilis
+              quo. Ducimus repellendus distinctio quia! Recusandae suscipit,
+              repellendus eum ipsum neque sequi dolorum f ugiat, explicabo
+              assumenda vero accusantium consequatur animi?
+            </p>
+          </div>
 
-            <div className="text">
-              <h3>Header Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-                quo quia nulla maxime rerum odit vitae quisquam aspernatur eos
-                placeat repellat nobis nisi exercitationem deleniti alias id.
-                Asperiores, earum molestias. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Autem, facilis quo. Ducimus
-                repellendus distinctio quia! Recusandae suscipit, repellendus
-                eum ipsum neque sequi dolorum f ugiat, explicabo assumenda vero
-                accusantium consequatur animi?
-              </p>
-            </div>
+          <div className="text">
+            <h3>Header Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              quo quia nulla maxime rerum odit vitae quisquam aspernatur eos
+              placeat repellat nobis nisi exercitationem deleniti alias id.
+              Asperiores, earum molestias. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Autem, facilis quo. Ducimus
+              repellendus distinctio quia! Recusandae suscipit, repellendus eum
+              ipsum neque sequi dolorum f ugiat, explicabo assumenda vero
+              accusantium consequatur animi?
+            </p>
           </div>
-          <hr />
-          <Content />
-          <hr />
-          <div className="latest-blogs">
-            <h2>Latest From Our Blog</h2>
-          </div>
-          {postList.edges.map(({ node }, i) => (
-            <Link to={node.fields.slug} className="link">
-              <div className="post-list">
-                <h2>{node.frontmatter.title}</h2>
-                <span>{node.frontmatter.date}</span>
-                <p>{node.excerpt}</p>
-              </div>
-            </Link>
-          ))}
         </div>
-        <br />
-        <br />
-        <TopFooter />
-        <script>
-          if (window.netlifyIdentity){' '}
-          {window.netlifyIdentity.on('init', function user() {
-            if (!user) {
-              window.netlifyIdentity.on('login', function() {
-                document.location.href = '/admin/'
-              })
-            }
-          })}
-        </script>
-      </body>
+        <hr />
+        <Content />
+        <hr />
+        <div className="latest-blogs">
+          <h2>Latest From Our Blog</h2>
+        </div>
+        {postList.edges.map(({ node }, i) => (
+          <Link to={node.fields.slug} className="link">
+            <div className="post-list">
+              <h2>{node.frontmatter.title}</h2>
+              <span>{node.frontmatter.date}</span>
+              <p>{node.excerpt}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <br />
+      <br />
+      <TopFooter />
     </Layout>
   )
 }
